e0b49bc503fcf5e026938df66dc3350da7907bbc
mukundftbl <mukundftbl@gmail.com>
2026-07-24 02:38:34 +0530

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tasty_Burger-React/',
-})
+  base: "/Tasty_Burger-React/",
+});
